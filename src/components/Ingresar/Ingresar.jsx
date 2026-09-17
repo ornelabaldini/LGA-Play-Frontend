8323b6df4a84c4307895a346b6e6cbc9f350579f
Ornela <ornelabaldin@gmail.com>
2026-09-16 21:34:03 -0300

    Stock

---
+++ src/components/Ingresar/Ingresar.jsx
@@ -0,0 +1,24 @@
+﻿import "./Ingresar.css";
+
+function Ingresar({ esAdmin, setEsAdmin }) {
+  const ingresar = () => {
+    setEsAdmin(true);
+  };
+
+  const cerrarSesion = () => {
+    setEsAdmin(false);
+  };
+
+  return (
+    <>
+      <button
+        className="ingresar"
+        onClick={esAdmin ? cerrarSesion : ingresar}
+      >
+        {esAdmin ? "Cerrar sesión" : "Ingresar"}
+      </button>
+    </>
+  );
+}
+
+export default Ingresar;
